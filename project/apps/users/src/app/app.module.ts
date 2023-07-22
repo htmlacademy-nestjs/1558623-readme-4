b8101d77b509c9c4e1013e2a86fa4f5d50cb9157
Andrey_Abramov <i.andrei.abramov@gmail.com>
2023-07-22 18:22:33 +0300

    blog-user & authentication modules created

--- project/apps/users/src/app/app.module.ts
+++ project/apps/users/src/app/app.module.ts
@@ -1,9 +1,10 @@
-import {Module} from '@nestjs/common';
+import { Module } from '@nestjs/common';
+import { BlogUserModule } from './blog-user/blog-user.module';
+import { AuthenticationModule } from './authentication/authentication.module';
 
 @Module({
-  imports: [],
+  imports: [BlogUserModule, AuthenticationModule],
   controllers: [],
   providers: [],
 })
-export class AppModule {
-}
+export class AppModule {}
