4b9f22edf78d5bda9c9bf24f5fd674e39e53bfca
Andrey_Abramov <i.andrei.abramov@gmail.com>
2023-07-22 20:40:47 +0300

    setPassword method added to BlogUserEntity

---
+++ project/apps/users/src/app/blog-user/blog-user.entity.ts
@@ -0,0 +1,42 @@
+import { IUser, TUserRole } from '@project/shared/app-types';
+import { genSalt, hash } from 'bcrypt';
+import { SALT_ROUNDS } from './blog-user.constants.js';
+
+export class BlogUserEntity implements IUser {
+  public _id: string;
+  public name: string;
+  public email: string;
+  public passwordHash: string;
+  public role: TUserRole;
+  public avatar: string;
+
+  constructor(user: IUser) {
+    this.fillEntity(user);
+  }
+
+  public toObject(): IUser {
+    return {
+      _id: this._id,
+      name: this.name,
+      email: this.email,
+      passwordHash: this.passwordHash,
+      role: this.role,
+      avatar: this.avatar
+    };
+  }
+
+  public fillEntity({ _id, name, email, passwordHash, role, avatar }: IUser) {
+    this._id = _id;
+    this.name = name;
+    this.email = email;
+    this.passwordHash = passwordHash;
+    this.role = role;
+    this.avatar = avatar;
+  }
+
+  public async setPassword(password: string): Promise<BlogUserEntity> {
+    const salt = await genSalt(SALT_ROUNDS);
+    this.passwordHash = await hash(password, salt);
+    return this;
+  }
+}
