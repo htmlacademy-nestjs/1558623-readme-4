f9df1019fa4858373379abffedc99e98ecd02c2c
Andrey_Abramov <i.andrei.abramov@gmail.com>
2023-07-23 01:17:11 +0300

    verifyUser method created in auth service

--- project/apps/users/src/app/authentication/authentication.constants.ts
+++ project/apps/users/src/app/authentication/authentication.constants.ts
@@ -1,3 +1,5 @@
 export const AuthMessage = {
-  UserExists: (email: string): string => `User with email ${email} already exists`
-};
+  UserExists: (email: string): string => `User with email ${email} already exists`,
+  UserNotFound: (email: string): string => `User with email ${email} not found`,
+  PasswordIncorrect: () => 'Password is incorrect'
+} as const;
--- project/apps/users/src/app/authentication/authentication.service.ts
+++ project/apps/users/src/app/authentication/authentication.service.ts
@@ -1,9 +1,10 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { BlogUserMemoryRepository } from '@blog-user/blog-user.memory-repository.js';
 import { CreateUserDto } from './dto/create-user.dto.js';
 import { AuthMessage } from './authentication.constants.js';
 import { BlogUserEntity } from '@blog-user/blog-user.entity.js';
 import { IUser } from '@project/shared/app-types';
+import { LoginUserDto } from './dto/login-user.dto.js';
 
 @Injectable()
 export class AuthenticationService {
@@ -12,6 +13,25 @@ export class AuthenticationService {
 
   public async getUserById(id: string): Promise<IUser> {
     return this.blogUserRepository.findById(id);
+  }
+
+  public async verifyUser(dto: LoginUserDto): Promise<IUser> {
+    const { email, password } = dto;
+
+    const existingUser = await this.blogUserRepository.findByEmail(email);
+
+    if (existingUser) {
+      throw new NotFoundException(AuthMessage.UserNotFound(email));
+    }
+
+    const blogUserEntity = new BlogUserEntity(existingUser);
+    const isPasswordCorrect = await blogUserEntity.comparePasswordWithHash(password);
+
+    if (!isPasswordCorrect) {
+      throw new UnauthorizedException(AuthMessage.PasswordIncorrect());
+    }
+
+    return blogUserEntity.toObject();
   }
 
   public async register(dto: CreateUserDto) {
