6b3ba120a6c746a8f71f2443c80e0642b0013e7a
Andrey_Abramov <i.andrei.abramov@gmail.com>
2023-07-23 17:47:47 +0300

    ApiProperties configured for user dtos

--- project/apps/users/src/app/authentication/dto/create-user.dto.ts
+++ project/apps/users/src/app/authentication/dto/create-user.dto.ts
@@ -1,6 +1,34 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
+  @ApiProperty({
+    description: 'User name',
+    minLength: 3,
+    maxLength: 50,
+    required: true,
+  })
   name: string;
+
+  @ApiProperty({
+    description: 'Valid user email',
+    example: 'faker@fake.co',
+    uniqueItems: true,
+    required: true,
+  })
   email: string;
+
+  @ApiProperty({
+    description: 'Password',
+    minLength: 6,
+    maxLength: 12,
+    required: true,
+  })
   password: string;
+
+  @ApiProperty({
+    description: 'User avatar path',
+    example: 'user/avatar.jpg',
+    required: false,
+  })
   avatar?: string;
 }
--- project/apps/users/src/app/authentication/dto/login-user.dto.ts
+++ project/apps/users/src/app/authentication/dto/login-user.dto.ts
@@ -1,4 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class LoginUserDto {
+  @ApiProperty({
+    description: 'Valid user email',
+    example: 'faker@fake.co',
+    uniqueItems: true,
+    required: true,
+  })
   email: string;
+
+  @ApiProperty({
+    description: 'Password',
+    minLength: 6,
+    maxLength: 12,
+    required: true,
+  })
   password: string;
 }
