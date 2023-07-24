70a4320040e2aa01b13fb5d2dcd676b50fe7027c
Andrey_Abramov <i.andrei.abramov@gmail.com>
2023-07-24 20:30:07 +0300

    blog modules generated

--- project/apps/blog/src/app/app.module.ts
+++ project/apps/blog/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PostsModule } from './posts/posts.module';
+import { CommentsModule } from './comments/comments.module';
+import { FeedModule } from './feed/feed.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PostsModule, CommentsModule, FeedModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
