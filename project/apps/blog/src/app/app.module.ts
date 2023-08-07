54427d3b41a395acafcaff5847ef4c5365a03c25
Andrey_Abramov <i.andrei.abramov@gmail.com>
2023-08-07 12:47:15 +0300

    PrismaModule and PrismaService created

--- project/apps/blog/src/app/app.module.ts
+++ project/apps/blog/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PostsModule } from './posts/posts.module';
 import { CommentsModule } from './comments/comments.module';
 import { FeedModule } from './feed/feed.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [PostsModule, CommentsModule, FeedModule],
+  imports: [PostsModule, CommentsModule, FeedModule, PrismaModule],
   controllers: [],
   providers: [],
 })
