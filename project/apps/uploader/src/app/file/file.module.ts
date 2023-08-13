684ca342af763af865075cb433c9325523d05028
Andrey_Abramov <i.andrei.abramov@gmail.com>
2023-08-13 16:15:46 +0300

    ServeStaticModule configured

--- project/apps/uploader/src/app/file/file.module.ts
+++ project/apps/uploader/src/app/file/file.module.ts
@@ -1,8 +1,30 @@
 import { Module } from '@nestjs/common';
 import { FileController } from './file.controller';
 import { FileService } from './file.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { ConfigService } from '@nestjs/config';
+
+const SERVE_ROOT = '/static';
 
 @Module({
+  imports: [
+    ServeStaticModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const rootPath = configService.get('uploader.uploadDirectory');
+        return [
+          {
+            rootPath,
+            serveRoot: SERVE_ROOT,
+            serveStaticOptions: {
+              fallthrough: true,
+              etag: true,
+            }
+          },
+        ];
+      },
+    }),
+  ],
   controllers: [FileController],
   providers: [FileService],
 })
--- project/apps/uploader/src/app/file/file.service.ts
+++ project/apps/uploader/src/app/file/file.service.ts
@@ -4,16 +4,25 @@ import { ConfigType } from '@nestjs/config';
 import { Express } from 'express';
 import { ensureDir } from 'fs-extra';
 import { writeFile } from 'node:fs/promises';
+import dayjs from 'dayjs';
+import * as crypto from 'crypto';
+import { extension } from 'mime-types';
 
 @Injectable()
 export class FileService {
   constructor(
-    @Inject(uploaderConfig.KEY) private readonly config: ConfigType<typeof uploaderConfig>,
+    @Inject(uploaderConfig.KEY)
+    private readonly config: ConfigType<typeof uploaderConfig>,
   ) {}
 
   public async writeFile(file: Express.Multer.File) {
-    const uploadDirectoryPath = this.config.uploadDirectory;
-    const filePath = `${uploadDirectoryPath}${file.originalname}`;
+    const [year, month] = dayjs().format('YYYY MM').split(' ');
+    const { uploadDirectory } = this.config;
+    const fileName = crypto.randomUUID();
+    const fileExtension = extension(file.mimetype);
+
+    const uploadDirectoryPath = `${uploadDirectory}/${year}/${month}`;
+    const filePath = `${uploadDirectoryPath}${fileName}.${fileExtension}`;
 
     await ensureDir(uploadDirectoryPath);
     await writeFile(filePath, file.buffer);
